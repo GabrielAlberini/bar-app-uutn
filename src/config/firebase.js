3aa69a284642b0c10778d801a14fd26c6aafa630
gabrielalberini <gabialberini733@gmail.com>
2024-11-01 10:59:48 -0300

    primer commit para producción

---
+++ src/config/firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Configuración de Firebase proporcionada por Firebase Console
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
+};
+
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+
+// Inicializa Firestore
+const db = getFirestore(app);
+
+// Log para validar la conexión a la base de datos
+console.log("Conexión a Firestore establecida:", db);
+
+export default db;
